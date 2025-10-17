b0463cf46a1673eed43a390a03e76df7fb49d0bc
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2025-10-17 18:08:16 +0200

    first commit

---
+++ src/main/java/com/trinitarias/api_formulario/ApiFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.trinitarias.api_formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFormularioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinitarias/api_formulario/ApiFormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinitarias.api_formulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
