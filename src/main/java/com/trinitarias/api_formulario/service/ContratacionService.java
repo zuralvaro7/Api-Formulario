7cfa8854441353cf9fd40d93fd5895dd95353ce4
Marco Varas Pérez <marcovarasperez@gmail.com>
2025-11-10 09:02:40 +0100

    service terminado 1 vez

--- src/main/java/com/trinitarias/api_formulario/service/ContratacionService.java
+++ src/main/java/com/trinitarias/api_formulario/service/ContratacionService.java
@@ -1,5 +1,45 @@
 package com.trinitarias.api_formulario.service;
-
+@Service
 public class ContratacionService {
+    private ArrayList<ContratacionDto>lista=new ArrayList();
+    
+    //Obtener datos Dto
+    public ArrayList<ContratacionDto> obtener(){
+        return lista;
+    }
+    //Obtener un solo Dto
+    public ContratacionDto verId(int id){
+        for (ContratacionDto c:lista){
+            if(c.getId().equals(id)){
+                return c;
+            }
+        }
+        return null;
+    }
+    //Crear Dto
+    public void crear(ContratacionDatosBancarios datosbancarios, ContratacionDatosContacto datosContacto, ContratacionDatosPersonales datosPersonales, ContratacionDireccion direccion, ContratacionMetodoPago metodoPago){
+        ContratacionDto cdto = new ContratacionDto(datosbancarios,datosContacto,datosPersonales,direccion,metodoPago);
+        lista.add(cdto);
 
+    }
+    //Actualizar Dto
+    public void actualizar(int id, ContratacionDto cd){
+        for(ContratacionDto c lista){
+            if(c.getId().equals(id)){
+                c.setDatosBancarios(datosbancarios);
+                c.setDatosContacto(datosContacto);
+                c.setDatosPersonales(datosPersonales);
+                c.setDireccion(direccion);
+                c.setMetodoPago(metodoPago);
+            }
+        }
+    }
+    //Borrar
+    public void Borrar(int id){
+        for (ContratacionDto c:lista){
+            if(c.getId().equals(id)){
+                lista.remove(c);
+            }
+        }
+    }
 }
