978e082bfa77a0f16d6a3f47fa5ba1620ab20361
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2025-11-10 09:05:07 +0100

    Cambios en los metodos, añadido mensajes de error

--- src/main/java/com/trinitarias/api_formulario/controller/ContratacionController.java
+++ src/main/java/com/trinitarias/api_formulario/controller/ContratacionController.java
@@ -1,5 +1,6 @@
 package com.trinitarias.api_formulario.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,34 +13,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trinitarias.api_formulario.dto.ContratacionDto;
+import com.trinitarias.api_formulario.service.ContratacionService;
 
 @RestController
 @RequestMapping("/api/v1/contratacion")
 public class ContratacionController {
-
+	
+	@Autowired
+	ContratacionService service;
+	
 	@GetMapping()
 	public ResponseEntity<?> listarTodo(){
-		return ResponseEntity.status(HttpStatus.OK).body(null);
+		ContratacionDto c;
+		return ResponseEntity.status(HttpStatus.OK).body(c);
+		
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<?> listarId(@PathVariable Long id){
-		return ResponseEntity.status(HttpStatus.OK).body(null);
+		ContratacionDto c;
+		if(true) {
+			return ResponseEntity.status(HttpStatus.OK).body(c);			
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
 	}
 	
 	@PostMapping
 	public ResponseEntity<?> crearContratacion(@RequestBody ContratacionDto contratacionDto){
-		return ResponseEntity.status(HttpStatus.CREATED).body(null);
+		ContratacionDto c;
+		if(true) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(c);
+		} else {
+			return ResponseEntity.status(HttpStatus.CREATED).body(c);
+		}
 	}
 	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> actualizarContratacion(@PathVariable Long id, @RequestBody ContratacionDto contratacionDto){
-		return ResponseEntity.status(HttpStatus.OK).body(null);
+		ContratacionDto c;
+		if(true) {
+			return ResponseEntity.status(HttpStatus.OK).body(c);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(c);
+		}
+		
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> borrarContratacion(@PathVariable Long id, @RequestBody ContratacionDto contratacionDto){
-		return ResponseEntity.status(HttpStatus.OK).body(null);
+		ContratacionDto c;
+		if(true) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(c);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
 	}
 	
 }
