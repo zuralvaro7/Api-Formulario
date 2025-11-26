b321169208ef1706984abe82902874221864365d
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2025-11-26 09:33:59 +0100

    Cambiado el nombre de la tabla

--- src/main/java/com/trinitarias/api_formulario/entity/ContratacionEntity.java
+++ src/main/java/com/trinitarias/api_formulario/entity/ContratacionEntity.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="metodoPago")
+@Table(name="contrataciones")
 public class ContratacionEntity {
 
 	//ID AUTOGENERADO
