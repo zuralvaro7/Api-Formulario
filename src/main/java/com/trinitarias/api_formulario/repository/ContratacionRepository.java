8b0141d08737dcab896137853cc49d0032cc3f05
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2025-11-27 12:28:15 +0100

    Cambio por el ContratacionDto a ContratacionEntity

--- src/main/java/com/trinitarias/api_formulario/repository/ContratacionRepository.java
+++ src/main/java/com/trinitarias/api_formulario/repository/ContratacionRepository.java
@@ -2,8 +2,8 @@ package com.trinitarias.api_formulario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.trinitarias.api_formulario.dto.ContratacionDto;
+import com.trinitarias.api_formulario.entity.ContratacionEntity;
 
-public interface ContratacionRepository extends JpaRepository<ContratacionDto, Long>{
+public interface ContratacionRepository extends JpaRepository<ContratacionEntity, Long>{
 
 }
